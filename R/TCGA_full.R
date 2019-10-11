5a6d5c8ab53c0faae7da3fdc20474d36ae55a918
Zequn Sun <sunzequn1@gmail.com>
2019-10-11 01:58:44 -0400

    Add files via upload

---
+++ R/TCGA_full.R
@@ -0,0 +1,15 @@
+#' @title Full TCGA data
+#'
+#' @description Full TCGA data 
+#' @name TCGA_full
+#' @docType data
+#' @usage TCGA_full
+#' @format A \code{list} object with four elements, \code{geneexpr} (z scores for gene expression data), \code{t} (survival time), \code{d} (survival status indicator), \code{pathList} (pathway information).
+#'
+#' @references TCGA_full
+#' @keywords datasets
+#'
+#' @examples
+#' data(TCGA_full)
+
+NULL
